a92dd0790d1eb9c482d147ed4a2fa13350b66a7a
Matthew Franklin <matheist76@westnet.com.au>
2014-06-15 01:24:02 +1000

    * So, it helps if the cuts work  * Check that the operator isn't asleep when entering cuts by prompting repeatedly until we know what he's saying

--- encoding/lib/job.py
+++ encoding/lib/job.py
@@ -7,7 +7,6 @@ from xml.dom import minidom
 
 def create_mlt(talk, output_file):
     mlt = Element('mlt')
-    playlist = SubElement(mlt, "playlist", id="playlist0")
     for cut_file in talk["cut_list"]:
         producer = SubElement(mlt, 'producer', id=cut_file["filename"])
         producer_property = SubElement(producer, "property", name="resource")
@@ -18,6 +17,9 @@ def create_mlt(talk, output_file):
             args['in'] = str(int(cut_file['in'].total_seconds()) * 25)
         if 'out' in cut_file and cut_file['out']:
             args['out'] = str(int(cut_file['out'].total_seconds()) * 25)
+
+    playlist = SubElement(mlt, "playlist", id="playlist0")
+    for cut_file in talk["cut_list"]:
         playlist_entry = SubElement(playlist, "entry", args)
     ElementTree.ElementTree(mlt).write(output_file)
 
