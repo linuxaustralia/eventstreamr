5782cdb947e0b0c979cb0e9fba6ae5ab67149c69
Matthew Franklin <matheist76@westnet.com.au>
2014-06-14 14:53:22 +1000

    Added the duration func to lib/duration.py. Added a new string format str2delta to deal with getting the duration

--- encoding/lib/duration.py
+++ encoding/lib/duration.py
@@ -1,4 +1,16 @@
 import datetime
+import subprocess
+
+
+def get_duration(filename):
+# from matt, untested since i don't have test files
+# not quite sure what this returns/outputs
+    cmd = "exiftool -duration " + filename
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    for line in p.stdout:
+        duration = line.split()[2]
+    return str2delta(duration)
+
 
 def str2delta(raw):
 	formats = ["%H:%M:%S", "%H:%M:%S.%f", "%M:%S", "%M:%S.%f", "%S.%f"]
