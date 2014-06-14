8e9eba55d05eeec246e3a851d598a5752e7184ad
Matthew Franklin <matheist76@westnet.com.au>
2014-06-14 14:43:16 +1000

    Just added a new time format in duration.py

--- encoding/lib/duration.py
+++ encoding/lib/duration.py
@@ -1,7 +1,7 @@
 import datetime
 
 def str2delta(raw):
-	formats = ["%H:%M:%S", "%H:%M:%S.%f", "%M:%S", "%M:%S.%f"]
+	formats = ["%H:%M:%S", "%H:%M:%S.%f", "%M:%S", "%M:%S.%f", "%S.%f"]
 	time = None
 	zero = datetime.datetime.min
 	for f in formats:
