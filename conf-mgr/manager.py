189f2864c2d20a1128e0cea289a84ef93068e54b
Lee Symes <leesdolphin@users.sourceforge.net>
2014-07-17 13:13:55 +1000

    Implemented the encoding queue in the root files. On a side note - IT WORKS - kinda.

--- conf-mgr/manager.py
+++ conf-mgr/manager.py
@@ -1,26 +1,115 @@
 __author__ = 'Lee Symes'
 
+
 from twisted.internet import reactor, task
 from twisted.internet.protocol import Factory
+from twisted.protocols.amp import AMP
 
-from lib.json_helper import load_json
-from lib.monitor import FilteredFileInFolderMonitor
+from lib import general_commands
+from lib.commands import ConfiguredCommandAMP
+from lib.file_helper import load_json
+from lib.monitor import FilteredFilesInFolderMonitor
+from lib.manager import get_queue_directories
 from lib.manager.queue import EncoderQueue
-from roles import SuperAMP
+
 
 # Load the config as a dictionary from the JSON file.
 manager_config = load_json("manager.json")
+print manager_config
 
-registered_stations = []
+#
+registered_stations = {}
+"""
+A Dictionary mapping between the ip/port tuple and the station's config. This will never contain empty values.
+Once a station disconnects it's information is removed from this dictionary.
+
+:type : dict[tuple[string,int],StationInformation]
+"""
+
+class StationInformation:
+    def __init__(self, station_transport, station_config=None):
+        """
+
+        :type station_transport: AMP
+        :type station_config: lib.config.StationConfig
+        """
+        self.station_transport = station_transport
+        self.station_config = station_config
+
+    def __str__(self):
+        return "%s -> %s" % (self.station_transport, self.station_config)
+
+
+class DeadStationRemoverAMP(ConfiguredCommandAMP):
+    """
+    This class monitors connection and disconnection
+
+    """
+
+    def startReceivingBoxes(self, boxSender):
+        """
+        self.transport.client -> tuple(ip_address, port)
+        """
+        print boxSender
+        registered_stations[self.transport.client] = StationInformation(boxSender)
+        ConfiguredCommandAMP.startReceivingBoxes(self, boxSender)
+
+    def sendBox(self, box):
+        print "Sending:  ", box
+        super(DeadStationRemoverAMP, self).sendBox(box)
+
+    def ampBoxReceived(self, box):
+        print "Received: ", box
+        super(DeadStationRemoverAMP, self).ampBoxReceived(box)
+
+    def stopReceivingBoxes(self, reason):
+        """
+        No further boxes will be received on this connection.
+
+        @type reason: L{Failure}
+        """
+        # The transport.client will give a unique tuple which we can link up to a registered station and then remove
+        # because it's disconnecting.
+        client = self.transport.client
+        print "Disconnecting from     ", client
+        print "Discarded Information: ", registered_stations.pop(client, "NO INFORMATION STORED")
+        registered_stations.pop(client, {})
+        ConfiguredCommandAMP.stopReceivingBoxes(self, reason)
+
+
+@general_commands.manager_commands.responder(general_commands.RegisterStationCommand)
+def register_station(config, transport):
+    client = transport.client
+    if client not in registered_stations:
+        registered_stations[client] = StationInformation(transport)
+    info = registered_stations[client]
+
+    info.station_config = config
+
+    role_config = {"encode":
+                       {"uuid": "encode",
+                        "command": "echo"
+                       }}
+
+    info.station_config.roles = role_config
+
+    return {"config": role_config}
 
 
 def configure_encoder_fs_watch():
-    encode_config = manager_config["encode"]
+    if "encode" not in manager_config["queues"]:
+        print "Inside check failed. ", manager_config["queues"].iterkeys()
+        # No configuration for encoding.
+        return
+    encode_config = manager_config["queues"]["encode"]
     base_path = encode_config["base_path"]
-    check_folder = base_path + "todo"
-    file_pattern = encode_config["file_pattern"]
-    l = task.LoopingCall(FilteredFileInFolderMonitor(check_folder, file_pattern, EncoderQueue(base_path)))
-    l.start(10.0, now=False) # Call every minute. Wait a minute before starting.
+    todo_path = get_queue_directories(base_path, "todo")
+    encode_queue = EncoderQueue(encode_config, registered_stations)
+
+    l = task.LoopingCall(FilteredFilesInFolderMonitor(todo_path,
+                                                      encode_config["file_pattern"],
+                                                      encode_queue))
+    l.start(10.0, now=True) # Call every minute. Wait a minute before starting.
 
 
 
@@ -33,18 +122,23 @@ def configure_encoder_fs_watch():
 
 
 
+def create_and_configure_AMP():
+    print "Configure"
+    amp = DeadStationRemoverAMP()
+    return amp
 
 
 def main():
+    general_commands.manager_commands.register()
     # FROM: http://twistedmatrix.com/documents/current/core/examples/ampserver.py
 
     pf = Factory()
     pf.noisy = True
-    pf.protocol = SuperAMP
+    pf.protocol = create_and_configure_AMP
 
     configure_encoder_fs_watch()
 
-    reactor.listenTCP(8888, pf)
+    reactor.listenTCP(manager_config["listen_port"], pf)
     reactor.run()
 
 if __name__ == "__main__":
--- conf-mgr/station.py
+++ conf-mgr/station.py
@@ -1,41 +1,123 @@
-# FROM: http://twistedmatrix.com/documents/current/_downloads/ampclient.py
-import json_helper
-import os
+from twisted.internet.main import CONNECTION_DONE
+
+__author__ = "Lee Symes"
+
+
 from twisted.internet import reactor, task
+from twisted.internet.defer import inlineCallbacks
 from twisted.internet.endpoints import TCP4ClientEndpoint, connectProtocol
+from twisted.internet.error import ConnectError
 from twisted.protocols.amp import AMP
 
-from roles.encode import MessagingCommand
-from lib.json_helper import load_json
+from lib.commands import ConfiguredCommandAMP
+from lib.general_commands import RegisterStationCommand
+from lib.file_helper import load_json
+from lib.config import StationConfig
+import roles
 
 # Load the config as a dictionary from the JSON file.
-station_config = load_json("station.json")
+static_config = load_json("station.json")
 
-manager_endpoint = TCP4ClientEndpoint(reactor, station_config["manager_address"], station_config["manager_port"])
+role_config = StationConfig() # TODO load roles file.
+
+executing_roles = {}
+"""
+:type dict(string, roles.Role)
+"""
+
+manager_endpoint = TCP4ClientEndpoint(reactor, static_config["manager_address"], static_config["manager_port"])
+
+connection = None
+
+@inlineCallbacks
+def attempt_connection():
+    print "Attempting to connect..."
+    try:
+        conn = yield connectProtocol(manager_endpoint, ConfiguredCommandAMP())
+        global connection
+        connection = conn
+        yield register_station()
+    except ConnectError:
+        # Couldn't connect so try again in 30 seconds.
+        print "Failed to connect so I'll try again in a few seconds"
+        reactor.callLater(30, attempt_connection)
+    update_executing_roles()
 
 
-def doMath():
-    sumDeferred = connectProtocol(manager_endpoint, AMP())
-    def connected(ampProto):
-        print repr(ampProto)
-        r = ampProto.callRemote(MessagingCommand, msg="Hello World", time=1024)
-        print "c -", repr(r)
-        return r
-    sumDeferred.addCallback(connected)
-    def summed(result):
-        print repr(result)
-        r = result['resp']
-        print "s -", repr(r)
-        return r
-    sumDeferred.addCallback(summed)
+@inlineCallbacks
+def register_station():
+    print "Attempting to register station and update configs."
+    if not connection:
+        print "Called too early"
+        return
+
+    new_roles = yield connection.callRemote(RegisterStationCommand, config=role_config)
+    role_config.roles = new_roles["config"]
+
+    print "New Roles: ", repr(new_roles)
+    print "New Config:", repr(role_config)
 
 
-def doLooping():
+def update_executing_roles():
+    check_roles()
+    uuids = set()
+    for role, config in role_config.roles.iteritems():
+        factory = roles.get_factory(role)
+        if isinstance(config, (dict)):
+            uuids.add(init_role(factory, config))
+        else:
+            for config_item in config:
+                uuids.add(init_role(factory, config_item))
+
+    removed_uuids = set(executing_roles.iterkeys()) - uuids
+    for uuid in removed_uuids:
+        executing_roles[uuid].stop()
+
+
+def init_role(factory, config):
+    """
+
+
+    :type factory: roles.RoleFactory
+    :param factory:
+    :param config:
+    :raise Exception:
+    """
+    print "Starting configuration using config: ", config
+    print "Given Factory: ", factory
+    if "uuid" not in config or not config["uuid"]:
+        raise Exception("Invalid config. Must have a server defined UUID. Unable to continue.")
+    uuid = config["uuid"]
+    if uuid in executing_roles:
+        print "Updating `" + uuid + "`'s config"
+        executing_roles[uuid].update(config)
+    else:
+        print "Creating new with UUID=`" + uuid + "`"
+        new_role = factory.build(config)
+        executing_roles[uuid] = new_role
+    return uuid
+
+
+
+
+
+def check_roles():
+    new_roles = set(role_config.roles.iterkeys())
+    unconfigured_roles = new_roles - set(roles.get_factory_names())
+    if unconfigured_roles:
+        for role in unconfigured_roles:
+            print "ERR: Not loading `%s` as it is does not have a factory." % role
+            role_config.roles.pop(role) # Remove it to prevent it from saving.
+
+
+def do_roles_polling():
     pass
 
 def doRun():
-    doMath()
-    doLooping()
+    attempt_connection()
+    do_roles_polling()
     reactor.run()
+
+print dir(reactor)
 
 doRun()
