96fd7b6080e033275ec3d3642745ce8bc6088e4b
Lee Symes <leesdolphin@users.sourceforge.net>
2014-07-18 22:45:58 +1000

    Reworked `roles.__init__`.
    Removed `RoleConfig` as all configs are passed around as `dict` and any role that needs a special configuration object can create their own class.
    
    Changed `RoleFactory`:
     - Renamed `build` to `makeService` and it creates a new instance and configures it.
     - Created `build` to create a plain unconfigured instance of the `Role`. This is used by `makeService` to create a new instance.
     - Added a class variable to indicate the maximum number of instances that should be running.
    
    Removed `SingletonRole` and `SingletonRoleFactory` and replaced it with an instance count defined in `RoleFactory`.
    
    Changed `Role` to be a Service which means that implementers need to override `startService` and `stopService` for the setup and teardown code.

--- conf-mgr/roles/__init__.py
+++ conf-mgr/roles/__init__.py
@@ -1,22 +1,32 @@
 __author__ = 'Lee Symes'
-
-
 __doc__ = """
 
 This file defines the storage for the roles.
 
 """
 
+import pkgutil as _pkgutil
+import logging
+
+from twisted.application.service import Service
+
 __factories__ = {}
+
+_log = logging.getLogger("roles.__init__")
 
 
 def register_factory(name, factory):
     """Register the given factory as the given name. The factory object should be an initialised RoleFactory."""
+    _log.debug("Registering %s with the given factory: %r", name, factory)
     __factories__[name] = factory
 
 
 def get_factory(name):
-    """Get the factory for the name or None if no factory is registered under that name."""
+    """
+    Get the factory for the name or None if no factory is registered under that name.
+
+    :rtype: RoleFactory
+    """
     return __factories__.get(name, None)
 
 
@@ -25,75 +35,60 @@ def get_factory_names():
     return __factories__.iterkeys()
 
 
-class RoleConfig(object):
-
-    def __init__(self):
-        raise Exception("Please don't initialise the RoleConfig object directly")
-
-
 class RoleFactory(object):
+    """
+
+    :type instances: int
+    :cvar instances: The maximum number of instances that are allowed to be running at any given time. This should
+                      not be managed through this factory as it is managed higher in the chain. Set to 0 or less for
+                      any number of instances running at a given time.
+    """
+
+    instances = 0
 
     def __init__(self):
         pass
 
-    def build(self, given_config):
+    def makeService(self, given_config):
         """
         Start a new implementation of the role using given_config to configure it.
         This method should create a new role with the given configuration. If this method is unable to make a role
         using the given configuration then it should throw an exception.
 
-        This method should be implemented by subclasses.
+        This method's default operation is to create an instance using `build` and then configure it using
+        `Role.update`.
+        """
+        instance = self.build()
+        instance.update(given_config)
+
+        return instance
+
+    def build(self):
+        """
+        Implement this method to return a new role. The role will be configured using `Role.update`. If more advanced
+        configuration is needed, then implement `makeService`
+
+        :rtype: Role
         """
         return None
 
 
-class SingletonRoleFactory(RoleFactory):
+class Role(Service):
 
     def __init__(self):
-        super(SingletonRoleFactory, self).__init__()
-        self.instance = None
-
-    def build(self, given_config):
-        if self.instance is None or self.instance.is_stopped:
-            self.instance = self.build_singleton()
-
-        self.instance.update(given_config)
-
-        return self.instance
-
-    def build_singleton(self):
-        """
-
-        :rtype : SingletonRole
-        """
-        raise Exception("Implement build_singleton")
-
-
-class Role(object):
+        Service.__init__(self)
 
     def update(self, config):
         pass
 
-    def stop(self):
-        pass
 
-
-class SingletonRole(Role):
-
-    def __init__(self):
-        super(SingletonRole, self).__init__()
-        self.is_stopped = False
-
-    def stop(self):
-        super(SingletonRole, self).stop()
-        self.is_stopped = True
-
-
-import pkgutil as _pkgutil
+loaded = []
 for importer, module_name, is_pkg in _pkgutil.iter_modules(__path__):
     if not is_pkg and importer is not None:
-        print "Loading", module_name
+        _log.info("Loading `%s` module ...", module_name)
         __import__("%s.%s" % (__name__, module_name))
-        print "Loaded", module_name
+        _log.info("Loaded `%s` module.", module_name)
+        loaded.append(module_name)
 
-
+_log.info("Loaded the following modules: %r", loaded)
+del loaded
