b983dd201f05c1babd3ad5b8a17bc22fc760bfe8
Lee Symes <leesdolphin@users.sourceforge.net>
2014-07-18 21:06:17 +1000

    Added `lib.station.manager` to hold the services that maintain the currently running roles. This is code migrated out of `station` which has been refactored.

---
+++ conf-mgr/lib/station/manager.py
@@ -0,0 +1,176 @@
+from twisted.internet.defer import DeferredList
+
+__author__ = 'lee'
+
+import logging
+
+from twisted.application.service import MultiService
+
+from lib.exceptions import InvalidConfigurationException, BlockedRoleException, MissingRoleFactoryException
+from roles import get_factory, get_factory_names
+
+# Create a python logger for the manager
+_log = logging.getLogger("lib.station.manager")
+
+
+class NamedMultiService(MultiService):
+    """
+    This MultiService only accepts children with a name defined. It also adds a utility method to support removing
+    services by name without having to get the service first.
+    """
+
+    def addService(self, service):
+        if service.name is None:
+            raise NameError("Trying to add a service without a name. %r" % service)
+        if service.name not in get_factory_names():
+            raise NameError("Trying to add a service with a name that isn't a role. %r" % service)
+        MultiService.addService(self, service)
+
+    def removeNamedService(self, name):
+        return self.removeService(self.getServiceNamed(name))
+
+    def removeAllNamedServices(self, names, callback=None, add_name=True, *args, **kwargs):
+        deferred_list = []
+        for name in names:
+            stop_deferred = self.removeNamedService(name)
+            deferred_list.append(stop_deferred)
+            if callback:
+                if add_name:
+                    stop_deferred.addBoth(callback, name=name, *args, **kwargs)
+                stop_deferred.addBoth(callback, *args, **kwargs)
+        return DeferredList(deferred_list)
+
+
+class AllRolesManagerService(NamedMultiService):
+    """
+    """
+
+    def __init__(self, blocked_roles=None):
+        NamedMultiService.__init__(self)
+        self.blocked_roles = set(blocked_roles or [])
+
+    def configure(self, roles):
+        """
+        :type roles: dict[string, dict[string, object]]
+        """
+        self._check_all_roles_valid(roles)
+
+        existing_roles = set(self.namedServices.iterkeys())
+        updated_roles = set(roles.iterkeys())
+
+        self.removeAllNamedServices(existing_roles - updated_roles, self.role_manager_shutdown)
+
+        for role in updated_roles:
+            if role in existing_roles:
+                self.getServiceNamed(role).update_config(roles[role])
+            else:
+                new_manager = RoleManagerService(role)
+                new_manager.update_config(roles[role])
+                self.addService(new_manager)
+
+    def role_manager_shutdown(self, result, name):
+        _log.info("Completed shutdown of `%s`.", name)
+        _log.info("Result of shutdown: %r", result)
+        _log.info("Current roles registered: %r", self.namedServices.iterkeys())
+
+    def _check_all_roles_valid(self, roles):
+        """
+        Ensures that all roles can be configured by this station. This includes being on the given blacklist and not
+        having a factory. This method will throw a suitable subclass of `InvalidConfigurationException` if there are
+        any invalid roles.
+
+        This method also outputs descriptive information about each invalid role to the logger defined for this module.
+        """
+        new_roles = set(roles.iterkeys())
+        factories = set(get_factory_names())
+        blocked_roles = self.blocked_roles.intersection(new_roles)
+        if blocked_roles:
+            _log.error("The station was requested to run a role it is blocked from running.")
+            _log.error("Currently blocked roles: %r", self.blocked_roles)
+            for role in blocked_roles:
+                _log.error("`%s` is blocked from being loaded on this station", role)
+                _log.error("Configuration given for `%s`: %r", role, roles[role])
+                roles.pop(role)  # Remove it to prevent it from saving.
+            raise BlockedRoleException("Invalid config file received. See logs for more information. "
+                                       "Blocked Roles: %r" % blocked_roles)
+
+        missing_roles = new_roles - factories
+        if missing_roles:
+            _log.error("Failed to locate the following factories.")
+            _log.error("Currently defined factory names: %r", factories)
+            for role in missing_roles:
+                _log.error("`%s` does not have a factory configured.", role)
+                _log.error("Configuration given for `%s`: %r", role, roles[role])
+                roles.pop(role)  # Remove it to prevent it from saving.
+            raise MissingRoleFactoryException("Invalid config file received."
+                                              " Missing Roles: %r" % missing_roles)
+
+
+class RoleManagerService(NamedMultiService):
+    """
+    A MultiService that manages a single role's services. This means that all services in this object are all providing
+    an implementation of the same role.
+    """
+
+    def __init__(self, name):
+        NamedMultiService.__init__(self)
+        self.setName(name)
+
+    factory = property(lambda self: get_factory(self.name), doc="Returns the factory object for the given role.")
+
+    def update_config(self, config):
+        try:
+            mapped_config = _map_uuid_to_config(config)
+            existing_uuids = set(self.namedServices.iterkeys())
+            updated_uuids = set(mapped_config.iterkeys())
+
+            if len(updated_uuids) > self.factory.instances:
+                _log.error("Failed to update as there are too many new uuids. %d uuids > max %d instances",
+                           len(updated_uuids), self.factory.instances)
+                raise InvalidConfigurationException("Failed to update as there are too many new uuids. %d uuids >"
+                                                    " max %d instances" % (len(updated_uuids), self.factory.instances))
+
+            self.removeAllNamedServices(existing_uuids - updated_uuids, self.role_manager_shutdown)
+
+            for uuid in updated_uuids:
+                if uuid in existing_uuids:
+                    self.getServiceNamed(uuid).update(mapped_config[uuid])
+                else:
+                    service = self.factory.makeService(mapped_config[uuid])
+                    service.setName(uuid)
+                    self.addService(service)
+        except InvalidConfigurationException as e:
+            raise InvalidConfigurationException("Role{%s} -> %r" % (self.name, e))
+
+    def role_manager_shutdown(self, result, name):
+        _log.info("Completed shutdown of `%s`.", name)
+        _log.info("Result of shutdown: %r", result)
+        _log.info("Current uuids registered: %r", self.namedServices.iterkeys())
+
+
+def _map_uuid_to_config(config):
+    """
+    Returns a mapping from the uuid to the configuration(without uuid).
+
+    :rtype: dict[string, dict[string, object]]
+    """
+    from copy import deepcopy
+    if isinstance(config, dict):
+        try:
+            c = deepcopy(config)
+            uuid = c.pop("uuid")
+            return {uuid: c}
+        except KeyError as e:
+                raise InvalidConfigurationException("%r thrown whilst attempting to rearrange configuration:"
+                                                    " %r" % (e, config))
+    else:
+        d = {}
+        for cfg in config:
+            try:
+                c = deepcopy(cfg)
+                uuid = c.pop("uuid")
+                d[uuid] = c
+            except KeyError as e:
+                raise InvalidConfigurationException("%r thrown whilst attempting to rearrange configuration:"
+                                                    " %r" % (e, cfg))
+        return d
