d813adb8f5f836ff588c71750e13e94cb055a3df
Lee Symes <leesdolphin@users.sourceforge.net>
2014-07-20 23:45:25 +1000

    Figured out how to run scripts using Twisted's APIs.

---
+++ conf-mgr/Interesting Notes.md
@@ -0,0 +1,9 @@
+Running Python files using twisted
+----------------------------------
+
+Just add `#!/usr/bin/python` at the top of the file then to run simply:
+
+    reactor.spawnProcess(<<ProcessProtocol Instance>>, "path/to/script.py", ["path/to/script.py", ...])
+
+Where the `cwd` is the current directory of the current python process.
+This will also work with `.sh` files that also have the `#!` at the top.
---
+++ conf-mgr/scripts/run_encode.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+# It's late and I need to know that stuff is working - so why not tell a short story whilst I'm waiting.
+
+print "Hi world"
+print "My name is run_encode"
+print "I'm here to solve all the world's problems"
+print "By faking the encoding of something - I think"
+print "Don't ask too much. But what of my classified mission I can tell you is that my configuration is ..."
+print "..."
+print "..."
+print ""
+print ""
+from sys import argv
+print repr(argv)
+
+from time import sleep
+for i in xrange(1, 10):
+    print "I'm currently performing task %d of 10" % i
+    sleep(.5)
+
+print "All done. Now what ... ... ..."
+print "I need more sleep."
+print "G'Nite"
