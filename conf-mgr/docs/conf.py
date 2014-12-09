4c91ff431ea8bfbbb6c5447278d7ade222128a68
Lee Symes <leesdolphin@gmail.com>
2014-12-09 20:01:18 +1000

    Fixed RTD build.

--- conf-mgr/docs/conf.py
+++ conf-mgr/docs/conf.py
@@ -15,6 +15,8 @@
 import sys
 import os
 
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -114,11 +116,11 @@ html_theme = 'default'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-import sphinx_rtd_theme
+if on_rtd:
+  import sphinx_rtd_theme
 
-html_theme = "sphinx_rtd_theme"
-
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+  html_theme = "sphinx_rtd_theme"
+  html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
