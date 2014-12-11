005314ae37c2eff59154193ad83efd362d8d78e3
Lee Symes <leesdolphin@gmail.com>
2014-12-11 23:50:37 +1000

    Updated build scripts.

--- conf-mgr/build.py
+++ conf-mgr/build.py
@@ -2,14 +2,17 @@
 from __future__ import print_function
 
 import sys
+import os.path
 import subprocess
 
 def main():
     """
     This is the most horrible way I could have done this. Please forgive me.
     """
-    if "docs" in sys.argv:
-        subprocess.call(["pydoctor", "--make-html", "--project-name=Conference Manager",
+
+    if "api" in sys.argv:
+        subprocess.call(["pydoctor", "--html-output=-apidocs", "--make-html",
+                         "--project-name=Conference Manager",
                          "--add-package=lib", "--add-package=roles"])
     if "lint" in sys.argv:
         subprocess.call(["flake8", "--statistics", "lib", "roles", "scripts"])
@@ -17,10 +20,11 @@ def main():
         subprocess.call(["python", "-tt", "-B", "-m", "unittest", "discover"])
     if "tidy" in sys.argv:
         # Not lists because running in shell.
-        subprocess.call(["rm **/**.pyc"], shell=True)
-        subprocess.call(["rm **/.DS_Store"], shell=True)
+        # subprocess.call(["rm **/**.pyc"], shell=True)
+        # subprocess.call(["rm **/.DS_Store"], shell=True)
         subprocess.call(["rm", "-rf", "logs", "logs-temp"])
-
+    if "docs" in sys.argv:
+        subprocess.call(["make", "html"], cwd=os.path.abspath("docs/"))
 
 
 
