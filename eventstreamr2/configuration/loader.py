3f85b9ea87f34ee8a6bb8af2ebdf0a3ece7d8e75
Lee Symes <leesdolphin@gmail.com>
2014-12-19 12:47:20 +1000

    Loading of backup-style files completed and tested.

--- eventstreamr2/configuration/loader.py
+++ eventstreamr2/configuration/loader.py
@@ -1,19 +1,68 @@
-import eventstreamr2.configuration as cfg
-
 from eventstreamr2.lib.file_helper import load_json
 
+_undefined = object()
 
-def load_config_file(conf_mgr, file, source, priority = None):
+
+def load_config_file(conf_mgr, file, source, priority = _undefined):
     """
+    Load a single configuration from a file and store it in the given conference manager.
 
+    If ``priority`` is not given then it is loaded from the file(or defaults to ``None`` if it is missing). Otherwise it passes the value through to :meth:`set_config`.
+
+    :param conf_mgr: Configuration manager.
+    :type conf_mgr: eventstreamr2.configuration.ConfigurationManager
+    :param str file: The file name.
+    :param str source: The name of the configuration source to pass through to the manager.
+    :param source: The priority of the configuration source to pass through to the manager.
+    :type source: int or None
     """
     # If priority is None then load from file(key: _priority).
     # or leave to existing priority
 
     file_dict = load_json(file)
 
-    if priority is None:
+    if priority == _undefined:
         priority = file_dict.get("_priority", None)
 
     for service_name, config in file_dict.items():
         conf_mgr.set_config(service_name, source, priority, config)
+
+def load_backup_config_file(conf_mgr, file):
+    """
+    Load a single configuration from a file and store it in the given conference manager.
+
+    If ``priority`` is not given then it is loaded from the file(or defaults to ``None`` if it is missing). Otherwise it passes the value through to :meth:`set_config`.
+
+    It expects a file of the format::
+
+        {
+            "source": {
+                "_priority": 10
+                "service": {
+                    ...
+                },
+                "service2": {
+                    ...
+                }
+            }
+            "source2": {
+                "_priority": 10
+                ...
+            }
+        }
+
+    :param conf_mgr: Configuration manager.
+    :type conf_mgr: eventstreamr2.configuration.ConfigurationManager
+    :param str file: The file name.
+    :param str source: The name of the configuration source to pass through to the manager.
+    :param source: The priority of the configuration source to pass through to the manager.
+    :type source: int or None
+    """
+
+    file_dict = load_json(file)
+
+    for source, source_dict in file_dict.items():
+        if "_priority" in source_dict:
+            conf_mgr.set_source_priority(source, source_dict.pop("_priority"))
+        for service, service_cfg in source_dict.items():
+            conf_mgr.set_config(service, source, service_cfg)
--- tests/eventstreamr2/configuration/test_loader.py
+++ tests/eventstreamr2/configuration/test_loader.py
@@ -55,3 +55,32 @@ class load_config_fileTestCase(unittest.TestCase):
         self.assertEqual(self.mgr._configs[name][self.pk], 50)
         self.assertEqual(self.mgr._configs[name]["service1"]["complex"],
                     {"key1": "value", "key2": "value"})
+
+
+class load_backup_config_fileTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.mgr = cfg.ConfigurationManager()
+        self.pk = self.mgr._priority_key
+        self.path, _ = os.path.split(os.path.realpath(__file__))
+
+    def get_file(self, name):
+        return os.path.join(self.path, name)
+
+    def test_load_file(self):
+        fname = self.get_file("loader_backup_config.json")
+        print fname
+        loader.load_backup_config_file(self.mgr, fname)
+
+        self.assertIn("station", self.mgr._configs)
+        sc = self.mgr._configs["station"]
+        self.assertEqual(sc[self.pk], 100)
+        self.assertEqual(sc["web"], {"key": "value"})
+        self.assertEqual(sc["loader"], {"file": "somewhere.json"})
+
+
+        self.assertIn("manager", self.mgr._configs)
+        mc = self.mgr._configs["manager"]
+        self.assertNotIn(self.pk, mc)
+        self.assertEqual(mc["web"], {"key": "value1"})
+        self.assertEqual(mc["loader"], {"file": "somewhere-else.json"})
